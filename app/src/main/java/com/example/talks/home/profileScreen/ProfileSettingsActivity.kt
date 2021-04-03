044ad3d11e75b98db25d9e6dd680b43643313d42
yogesh-1305 <s.ashucool14@gmail.com>
2021-04-03 17:00:08 +0530

    profile settings tab, layout designed

--- app/src/main/java/com/example/talks/home/profileScreen/ProfileSettingsActivity.kt
+++ app/src/main/java/com/example/talks/home/profileScreen/ProfileSettingsActivity.kt
@@ -1,17 +1,24 @@
 package com.example.talks.home.profileScreen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import com.example.talks.R
 import com.example.talks.databinding.ActivityProfileSettingsBinding
+import com.r0adkll.slidr.Slidr
+import com.r0adkll.slidr.model.SlidrConfig
+import com.r0adkll.slidr.model.SlidrInterface
+import com.r0adkll.slidr.model.SlidrPosition
 
-class ProfileSettingsActivity : AppCompatActivity() {
+
+class ProfileSettingsActivity : FragmentActivity() {
 
     private lateinit var binding: ActivityProfileSettingsBinding
-
+    private lateinit var slidrInterface: SlidrInterface
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileSettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Slidr.attach(this)
     }
 }
