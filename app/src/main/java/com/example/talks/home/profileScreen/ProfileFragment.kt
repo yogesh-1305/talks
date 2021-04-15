a77924d8437e5552a7d78b3ecd57917cabfef342
yogesh-1305 <s.ashucool14@gmail.com>
2021-04-16 00:32:02 +0530

    profile settings layout changed

--- app/src/main/java/com/example/talks/home/profileScreen/ProfileFragment.kt
+++ app/src/main/java/com/example/talks/home/profileScreen/ProfileFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.widget.ImageView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.talks.R
@@ -41,13 +42,18 @@ class ProfileFragment : Fragment() {
             image = Encryption().decrypt(user1.profileImage, encryptionKey).toString()
             Glide.with(this).load(image).diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
                 .into(binding.profileScreenImage)
+            binding.phoneNumberInProfile.text = user1.phoneNumber
+            binding.usernameInProfile.text = user1.userName
         })
 
-        binding.profileScreenBackButton.setOnClickListener{
+        binding.profileScreenBackButton.setOnClickListener {
             activity?.finish()
         }
 
-
+        binding.editPhoneNumberCard.setOnClickListener {
+            Navigation.findNavController(binding.root)
+                .navigate(R.id.action_profileFragment2_to_profileEditFragment)
+        }
         return binding.root
     }
 
